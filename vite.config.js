6ca376bd1104392459e130a1a8474874d3a24c2d
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2026-01-20 03:32:54 +0100

    feat: PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import elmPlugin from 'vite-plugin-elm';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [
+    elmPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Sous Vide Calculator',
+        short_name: 'Sous Vide',
+        description: 'Baldwin Model Sous Vide Calculator',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: '/sous-vide-calculator/',
 });
